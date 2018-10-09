8ec983940961c4f4ac71226dd739c20082ab43f6
Temenuzhka Vuchova <tvuchova@gmail.com>
2018-10-09 12:46:38 +0300

    bernate simple examples`

---
+++ hibernate-demo/src/com/demo/jdbc/TestJdbc.java
@@ -0,0 +1,32 @@
+package com.demo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		String user = "devuser";
+		String pass = "Parola123456";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+	}
+
+}
+
+
+
